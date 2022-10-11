b92b1d238c10f4cc5954ee5f091e72077aa59a67
Keertiraj Malik <keertirajmalik@gmail.com>
2022-10-11 17:51:16 +0530

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Keertiraj Laxman Malik on 11/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
